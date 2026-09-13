015ae48f3a0556966880cfe7ec3b94dfcb2b1657
angelmicelti <angelmicelti@users.noreply.github.com>
2026-09-13 03:03:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'reducciones-v55';
+const CACHE_NAME = 'reducciones-v56';
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
+    './manual-administrador.pdf',
+    './manual-profesorado.pdf',
     './icons/favicon-16x16.png',
     './icons/favicon-32x32.png',
     './icons/icon-144x144.png',
